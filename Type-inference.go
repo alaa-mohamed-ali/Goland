2058866f2349815fa3d125a6445958f68ca70a8d
alaa-mohamed-ali <38326708+alaa-mohamed-ali@users.noreply.github.com>
2018-04-13 01:23:19 +0200

    Add files via upload

---
+++ Type-inference.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42 // change me!
+	fmt.Printf("v is of type %T\n", v)
+	//v is of type int
+
+	//fmt.Printf("v is of type %T : value %v\n", v, v)
+	//v is of type int : value 42
+}
