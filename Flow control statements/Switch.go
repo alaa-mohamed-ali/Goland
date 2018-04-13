1ed9a09ed51ed10a2c42871bc66a5caca5d30248
alaa-mohamed-ali <38326708+alaa-mohamed-ali@users.noreply.github.com>
2018-04-13 15:58:38 +0200

    Add files via upload

--- Flow control statements/Switch.go
+++ Flow control statements/Switch.go
@@ -1,12 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-)
+import "fmt"
 
 func main() {
-	fmt.Print("Go runs on ")
+	/*fmt.Print("Go runs on ")
 	switch os := runtime.GOOS; os {
 	case "darwin":
 		fmt.Println("OS X.")
@@ -16,5 +13,19 @@ func main() {
 		// freebsd, openbsd,
 		// plan9, windows...
 		fmt.Printf("%s.", os)
+	}*/
+
+	x := 42
+	switch x {
+	case 0:
+		fmt.Printf("no\n")
+	case 1:
+		fmt.Printf("no\n")
+	case 42:
+		fmt.Printf("yes\n")
+	case 43:
+		fmt.Printf("no\n")
+	default:
+		fmt.Printf("default\n")
 	}
 }
