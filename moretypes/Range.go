005093f324e291eab106b50a2b226eb65d9a7ca0
alaa-mohamed-ali <38326708+alaa-mohamed-ali@users.noreply.github.com>
2018-04-14 21:43:33 +0200

    Add files via upload

---
+++ moretypes/Range.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
+
+	/*
+		2**0 = 1
+		2**1 = 2
+		2**2 = 4
+		2**3 = 8
+		2**4 = 16
+		2**5 = 32
+		2**6 = 64
+		2**7 = 128
+	*/
+}
