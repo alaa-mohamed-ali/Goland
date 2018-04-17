30ea9d5df634b345b7bbf4075c30769641815cc6
alaa-mohamed-ali <38326708+alaa-mohamed-ali@users.noreply.github.com>
2018-04-17 15:00:56 +0200

    Add files via upload

--- RESTful/json.go
+++ RESTful/json.go
@@ -11,6 +11,7 @@ func main() {
 	content := "Hello ALaa TO GO LAND Progreming AAAAAAAA"
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(content)
 
 	})
