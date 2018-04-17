22a61fd3906f508c647aeeb6f849bfb63dcb203a
alaa-mohamed-ali <38326708+alaa-mohamed-ali@users.noreply.github.com>
2018-04-17 12:45:28 +0200

    Add files via upload

--- RESTful/RESTful.go
+++ RESTful/RESTful.go
@@ -8,8 +8,10 @@ import (
 )
 
 func main() {
+	content := "Hello ALaa TO GO LAND Progreming , %q"
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, content, html.EscapeString(r.URL.Path))
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
